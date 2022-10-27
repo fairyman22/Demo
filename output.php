43524d8fc1a6398702549c9b6a7986ae32ef0bf7
fairyman22 <nguyenhoangviet2204@gmail.com>
2022-10-27 09:51:54 +0700

    hihi

---
+++ output.php
@@ -0,0 +1,24 @@
+<?php
+    //echo dung de in ra man hinh chuoi va so co the noi voi nhau
+    echo "Hello World 123"," HAHA",23;
+    //print cung dung de in ra chuoi nhung k the noi dc voi nhau
+    print "Hello";
+    //print_r dung de in mang
+    print_r([2,3,4]);
+    print "Hello";
+    echo "Hello World 123"," HAHA",23;
+    //var_dump hien thi ra ca kieu du lieu va bien
+    var_dump(" hihih");
+    var_export("hihi");
+
+
+    //KHAI BA0 BIEN
+    $name = "Viet";
+    $age = 18;
+    $gpa = 3.54;
+
+    echo "Name: ".$name." Age: ".$age." GPA: ".$gpa;
+    echo "Name: $name Age: $age GPA: $gpa";
+
+
+?>
